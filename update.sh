10390e65aba3b96759ac134168fa74769f8c8a79
Sean Yap <sean@seanyap.com>
2024-10-31 23:04:53 +0800

    update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-cp -R hypr ~/.config/
-cp -R kitty ~/.config/
-cp -R mako ~/.config/
-cp -R waybar ~/.config/
-cp -R swaylock ~/.config/
-cp -R wofi ~/.config/
+cp -R ./config/ ~/.config/
